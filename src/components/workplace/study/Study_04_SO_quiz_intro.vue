e41c0e9730f52dcfa23ada0db841a6ebf5d281a0
gh.hwan <gh.hwan@d-insight.co.kr>
2018-12-14 14:15:05 +0900

    아이콘 추가 / 퀴즈인트로 / 핵심요약

---
+++ src/components/workplace/study/Study_04_SO_quiz_intro.vue
@@ -0,0 +1,55 @@
+<template>
+ <!-- eslint-disable -->
+    <!-- wrap -->
+    <div id="wrap" class="colorCode2">        
+      <!-- container -->
+      <div id="container" class="mediaquery study_quiz_int">
+            <!-- header -->
+            <div id="header">
+                <button type="button" class="btn only prev"><span class="ico">이전</span></button>
+                <div class="page-btn">
+                    <span class="ico back"></span>
+                    <div class="num"><span>03</span> / <span>36</span></div>
+                    <span class="ico next"></span>
+                </div>
+            </div>
+            <!-- //header -->
+            
+            <div id="content" class="etc">
+                <div class="cell">
+                    <div class="inner">
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- floatingbtn -->
+            <div class="absBtmBtn">  
+                <button type="button" class="btn">응시하기<span class="ico gray_next"></span></button>
+            </div>
+            <!-- //floatingbtn -->
+        </div>
+        <!-- //container --> 
+    </div>
+    <!-- //wrap -->
+
+</template>
+
+<script>
+export default {
+  name: 'Study_03_LO_viewer_fs',
+  /* vue lifecycle */
+  created () {
+  },
+  mounted () {
+  },
+  /* vue data */
+  data () {
+    return {
+    }
+  },
+  /* vue function */
+  methods: {
+  }
+}
+</script>
