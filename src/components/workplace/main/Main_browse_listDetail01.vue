d458e7e3099841c1c3c1c3dd465313fdcc8514f5
ssal07 <hm.kim@d-insight.co.kr>
2018-12-18 11:52:32 +0900

    browse 추가

--- src/components/workplace/main/Main_browse_listDetail01.vue
+++ src/components/workplace/main/Main_browse_listDetail01.vue
@@ -13,91 +13,91 @@
         <!-- //header -->
 
         <div id="content" class="list">
-        
-          <!-- //component -->
           <div class="cell">
-            <div class="scroll-x">
-                <div class="inner">
-                    <h2 class="h-tit2 mb10">경영일반 추천과정</h2>
-                    <!-- list -->
-                    <ul class="thumList">
-                        <li>
-                            <div class="in">
-                                <a href="#" class="thum">
-                                    <span class="label">정규</span>
-                                    <div class="thumbnail">
-                                        <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
-                                    </div>
-                                </a>
-                                <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
-                                <ul class="info">
-                                    <li class="color_clr3 fwb">이러닝</li>
-                                    <li><span class="star ico"></span>4.5</li>
-                                </ul>
-                                <div class="tagWrap"><span>교재</span><span>모바일병행</span></div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="in">
-                                <a href="#" class="thum">
-                                    <span class="label">정규</span>
-                                    <div class="thumbnail">
-                                        <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
-                                    </div>
-                                    <div class="tagWrap"></div>
-                                </a>
+            <div class="inner">
+                <div class="scroll-x type1">
+                    <div class="inner2">
+                        <h2 class="h-tit2 mb10">경영일반 추천과정</h2>
+                        <!-- list -->
+                        <ul class="thumList">
+                            <li>
+                                <div class="in">
+                                    <a href="#" class="thum">
+                                        <span class="label">정규</span>
+                                        <div class="thumbnail">
+                                            <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                        </div>
+                                    </a>
+                                    <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                                    <ul class="info">
+                                        <li class="color_clr3 fwb">이러닝</li>
+                                        <li><span class="star ico"></span>4.5</li>
+                                    </ul>
+                                    <div class="tagWrap"><span>교재</span><span>모바일병행</span></div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="in">
+                                    <a href="#" class="thum">
+                                        <span class="label">정규</span>
+                                        <div class="thumbnail">
+                                            <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                        </div>
+                                        <div class="tagWrap"></div>
+                                    </a>
 
-                                <a href="#" class="tit">기초부터 예제까지 iOS 개발자를 위한 업무에 바로 쓰는 스플...</a>
-                                <ul class="info">
-                                    <li class="color_clr3 fwb">이러닝</li>
-                                    <li><span class="star ico"></span>4.5</li>
-                                </ul>
-                                <div class="tagWrap"></div>
-                            </div>
-                        </li>
-                    </ul>
-                    <!-- //list -->
-                </div>
-                <div class="inner">
-                    <h2 class="h-tit2 mb10">경영전략 추천과정</h2>
-                    <!-- list -->
-                    <ul class="thumList">
-                        <li>
-                            <div class="in">
-                                <a href="#" class="thum">
-                                    <span class="label">정규</span>
-                                    <div class="thumbnail">
-                                        <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
-                                    </div>
-                                </a>
-                                <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
-                                <ul class="info">
-                                    <li class="color_clr3 fwb">이러닝</li>
-                                    <li><span class="star ico"></span>4.5</li>
-                                </ul>
-                                <div class="tagWrap"><span>교재</span><span>모바일병행</span></div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="in">
-                                <a href="#" class="thum">
-                                    <span class="label">정규</span>
-                                    <div class="thumbnail">
-                                        <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
-                                    </div>
+                                    <a href="#" class="tit">기초부터 예제까지 iOS 개발자를 위한 업무에 바로 쓰는 스플...</a>
+                                    <ul class="info">
+                                        <li class="color_clr3 fwb">이러닝</li>
+                                        <li><span class="star ico"></span>4.5</li>
+                                    </ul>
                                     <div class="tagWrap"></div>
-                                </a>
+                                </div>
+                            </li>
+                        </ul>
+                        <!-- //list -->
+                    </div>
+                    <div class="inner2">
+                        <h2 class="h-tit2 mb10">경영전략 추천과정</h2>
+                        <!-- list -->
+                        <ul class="thumList">
+                            <li>
+                                <div class="in">
+                                    <a href="#" class="thum">
+                                        <span class="label">정규</span>
+                                        <div class="thumbnail">
+                                            <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                        </div>
+                                    </a>
+                                    <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                                    <ul class="info">
+                                        <li class="color_clr3 fwb">이러닝</li>
+                                        <li><span class="star ico"></span>4.5</li>
+                                    </ul>
+                                    <div class="tagWrap"><span>교재</span><span>모바일병행</span></div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="in">
+                                    <a href="#" class="thum">
+                                        <span class="label">정규</span>
+                                        <div class="thumbnail">
+                                            <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                        </div>
+                                        <div class="tagWrap"></div>
+                                    </a>
 
-                                <a href="#" class="tit">기초부터 예제까지 iOS 개발자를 위한 업무에 바로 쓰는 스플...</a>
-                                <ul class="info">
-                                    <li class="color_clr3 fwb">이러닝</li>
-                                    <li><span class="star ico"></span>4.5</li>
-                                </ul>
-                                <div class="tagWrap"></div>
-                            </div>
-                        </li>
-                    </ul>
-                    <!-- //list -->
+                                    <a href="#" class="tit">기초부터 예제까지 iOS 개발자를 위한 업무에 바로 쓰는 스플...</a>
+                                    <ul class="info">
+                                        <li class="color_clr3 fwb">이러닝</li>
+                                        <li><span class="star ico"></span>4.5</li>
+                                    </ul>
+                                    <div class="tagWrap"></div>
+                                </div>
+                            </li>
+                        </ul>
+                        <!-- //list -->
+                    </div>
                 </div>
             </div>
           </div>
@@ -129,10 +129,7 @@
                 </ul>
             </div>
           </div>
-          <!-- //component -->
-
         </div>
-        <div class="btn-quickWrite"></div>
       </div>
       <!-- //container -->     
     </div>
@@ -141,7 +138,7 @@
 
 <script>
 export default {
-  name: 'Main_curateUI02',
+  name: 'Main_browse_listDetail01',
   /* vue lifecycle */
   created () {
   },
