e7a714aabbe8d20d7cd76fe547b1f2a2022d8427
eh0130 <eh.jung@d-insight.co.kr>
2018-12-24 13:08:28 +0900

    1224 customercenter 작업

---
+++ src/components/workplace/customercenter/CustomerCenter_Studysupport.vue
@@ -0,0 +1,78 @@
+<template>
+ <!-- eslint-disable -->
+    <!-- wrap -->
+    <div id="wrap" class="colorCode2">        
+        <!-- container -->
+        <div id="container" class="mediaquery drawer studysupport ">
+            
+            <!-- header -->
+            <div id="header" class="fixed">
+                <button type="button" class="btn only prev"><span class="ico">이전</span></button>
+                <p class="header_text font_18">학습지원</p>
+            </div>
+            <!-- //header -->
+
+            <div id="content" class="etc">
+                <div class="cell">
+                    <div class="inner nopad">
+                     <!-- list -->
+                        <p class="label-tit20"><strong>버전관리</strong></p>
+                        <ul class="singleList type_1">
+                            <li class="border_top">
+                                <a href="# pt0">버전정보<span class="version fr">최신 버전 v.0.1</span></a>
+                            </li>                           
+                        </ul>
+                        <p class="divide_line"></p>
+                        <p class="label-tit20"><strong>지원 모바일 환경</strong></p>
+                        <ul class="singleList type_1">
+                            <li>
+                                <a href="# pt0">원격지원 서비스 방법 안내<span class="black_next"></span></a>
+                            </li>
+                            <li>
+                                <a href="#">모바일 원격 접속 방법<span class="black_next"></span></a>
+                            </li>
+                        </ul>
+                        <p class="divide_line"></p>
+                        <p class="label-tit20"><strong>원격지원 서비스</strong></p>
+                        <ul class="singleList type_1">
+                            <li>
+                                <a href="# pt0">원격지원 서비스 방법 안내<span class="black_next"></span></a>
+                            </li>
+                            <li>
+                                <a href="#">모바일 원격 접속 방법<span class="black_next"></span></a>
+                            </li>
+                        </ul>
+                <!-- //list -->
+                       
+
+                       
+                    </div>
+                </div>
+            </div>           
+            
+        </div>
+        <!-- //container -->   
+
+    </div>
+    <!-- //wrap -->
+
+</template>
+
+<script>
+export default {
+  name: 'Drawer_myinfo',
+  /* vue lifecycle */
+  created () {
+  },
+  mounted () {
+  },
+  /* vue data */
+  data () {
+    return {
+    }
+  },
+  /* vue function */
+  methods: {
+  }
+}
+</script>
--- src/config/router/rootRoutes.js
+++ src/config/router/rootRoutes.js
@@ -214,6 +214,10 @@ import CustomerCenterRequest from '~workplace/customercenter/CustomerCenter_Requ
 import CustomerCenterRequestpopup1 from '~workplace/customercenter/CustomerCenter_Request_popup1'
 import CustomerCenterRequestpopup2 from '~workplace/customercenter/CustomerCenter_Request_popup2'
 import CustomerCenterRequesttoast01 from '~workplace/customercenter/CustomerCenter_Request_toast01'
+import CustomerCenterRequestresultlist from '~workplace/customercenter/CustomerCenter_Request_resultlist'
+import CustomerCenterPrivacyPolicy from '~workplace/customercenter/CustomerCenter_PrivacyPolicy'
+import CustomerCenterInquirydetail from '~workplace/customercenter/CustomerCenter_Inquiry_detail'
+import CustomerCenterStudysupport from '~workplace/customercenter/CustomerCenter_Studysupport'
 
 // 1번 영역
 var rootRoutes = [
@@ -1265,6 +1269,26 @@ var rootRoutes = [
     path: '/CustomerCenter_Request_toast01',
     name: 'CustomerCenter_Request_toast01',
     component: CustomerCenterRequesttoast01
+  },
+  {
+    path: '/CustomerCenter_Request_resultlist',
+    name: 'CustomerCenter_Request_resultlist',
+    component: CustomerCenterRequestresultlist
+  },
+  {
+    path: '/CustomerCenter_PrivacyPolicy',
+    name: 'CustomerCenter_PrivacyPolicy',
+    component: CustomerCenterPrivacyPolicy
+  },
+  {
+    path: '/CustomerCenter_Inquiry_detail',
+    name: 'CustomerCenter_Inquiry_detail',
+    component: CustomerCenterInquirydetail
+  },
+  {
+    path: '/CustomerCenter_Studysupport',
+    name: 'CustomerCenter_Studysupport',
+    component: CustomerCenterStudysupport
   }
 ]
 
