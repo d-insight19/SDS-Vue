c6184543b5991fb47942cc535ee77e3be86d7bbb
gh.hwan <gh.hwan@d-insight.co.kr>
2019-01-03 09:44:25 +0900

    merge

---
+++ src/components/workplace/eduperson/Eduperson_edugroup.vue
@@ -0,0 +1,65 @@
+<template>
+ <!-- eslint-disable -->
+    <!-- wrap -->
+    <div id="wrap" class="colorCode2">        
+      <!-- container -->
+      <div id="container" class="mediaquery eduperson_edugroup">
+
+        <div id="header" class="expand"> 
+            <div class="header_inner line1">
+                <p class="header_text font_18">멀티캠퍼스 교육관리 시스템</p>
+            </div>
+            <div class="hide_header">
+                <div class="edugroup_bg">
+                    <p class="edugroup_title">멀티캠퍼스</p>
+                    <p class="edugroup_sub_title">교육관리 시스템</p>
+                    <p class="edugroup_info">교육그룹을 선택하시면<br>관리시스템으로 이동합니다.</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="content" class="etc">
+          <div class="cell">
+            <div class="inner">
+                <ul class="ulList type1">
+                    <li>LEARNING GROUP TITLE 1<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 2<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 3<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 4<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 5<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 6<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 7<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 8<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 9<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 10<span class="ico next_gray_16"></span></li>
+                    <li>LEARNING GROUP TITLE 11<span class="ico next_gray_16"></span></li>
+                </ul>                               
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- //container -->    
+
+    </div>
+    <!-- //wrap -->
+
+</template>
+
+<script>
+export default {
+  name: 'Eduperson_edugroup',
+  /* vue lifecycle */
+  created () {
+  },
+  mounted () {
+  },
+  /* vue data */
+  data () {
+    return {
+    }
+  },
+  /* vue function */
+  methods: {
+  }
+}
+</script>
