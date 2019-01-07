75096efea3d6c7698156e59fec51eee2a4af71cc
choks0225 <choks0225@gmail.com>
2019-01-08 01:07:37 +0900

    ATOM header 작업중

--- src/components/workplace/common_page/components_mainheader.vue
+++ src/components/workplace/common_page/components_mainheader.vue
@@ -3,95 +3,221 @@
     <!-- wrap -->
     <div id="wrap" class="colorCode2">        
       <!-- container -->
-      <div id="container" class="mediaquery">
-        <!-- header 1단형 -->
-        <div id="header" class="mainType" style="display:none">
-            <button type="button" class="btn allMenu"><span class="ico">전체메뉴</span><span class="ico new"></span></button>
-            <ul class="gnbMenu">
-                <!-- selected class:on -->
-                <li class="menu menu_home on">
-                    <a href="#"><span class="ico">추천</span></a>
-                </li>
-                <li class="menu menu_exploration">
-                    <a href="#"><span class="ico">카테고리</span></a>
-                </li>
-                <li class="menu menu_subscription">
-                    <a href="#"><span class="ico">나의공간</span></a>
-                </li>
-            </ul>
-            <button type="button" class="btn btnSch"><span class="ico">검색</span></button>
-        </div>
-        <!-- //header -->
-
-        <!-- header 2단형 -->
-        <div id="header" class="mainType2" style="display:none1">
+      <div id="container" class="mediaquery sample_page">
+        
+        <!-- 메인헤더 1단형 -->
+        <h3>메인헤더 1단형 아이콘</h3>
+        <!-- header 1단형 아이콘 -->
+        <div id="header" class="mainType">
             <div class="header_inner line1">
-                <button type="button" class="btn allMenu"><span class="ico">전체메뉴</span><span class="ico new"></span></button>  
-                <span>ATORM</span>          
-                <button type="button" class="btn btnHome"><span class="ico">홈</span></button>
-            </div>            
-            <div class="header_inner line2">
+                <button type="button" class="btn allMenu"><span class="ico">전체메뉴</span><span class="ico new"></span></button>
                 <ul class="gnbMenu">
                     <!-- selected class:on -->
                     <li class="menu menu_home on">
-                        <a href="#"><span class="ico">추천</span></a>
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">추천</a>
                     </li>
                     <li class="menu menu_exploration">
-                        <a href="#"><span class="ico">카테고리</span></a>
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">탐색</a>
                     </li>
                     <li class="menu menu_subscription">
-                        <a href="#"><span class="ico">나의공간</span></a>
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">구독</a>
+                    </li>
+                    <li class="menu menu_learn">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">학습</a>
+                    </li>                
+                </ul>
+                <p class="main_tit_txt">마인드 피트니스</p>
+                <button type="button" class="btn btnSch"><span class="ico">검색</span></button>
+            </div>
+            <div class="header_inner line2">
+                <ul class="gnbMenu text">
+                    <!-- selected class:on -->
+                    <li class="menu menu_home on">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">추천</a>
+                    </li>
+                    <li class="menu menu_exploration">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">탐색</a>
+                    </li>
+                    <li class="menu menu_subscription">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">구독</a>
+                    </li>
+                    <li class="menu menu_learn">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">학습</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <h3>메인헤더 1단형 텍스트</h3>
+        <!-- header 1단형 텍스트 ::: mainType 에  txt클래스 추가 -->
+        <div id="header" class="mainType txt">
+            <div class="header_inner line1">
+                <button type="button" class="btn allMenu"><span class="ico">전체메뉴</span><span class="ico new"></span></button>
+                <ul class="gnbMenu">
+                    <!-- selected class:on -->
+                    <li class="menu menu_home on">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">추천</a>
+                    </li>
+                    <li class="menu menu_exploration">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">탐색</a>
+                    </li>
+                    <li class="menu menu_subscription">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">구독</a>
+                    </li>
+                    <li class="menu menu_learn">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">학습</a>
+                    </li>     
+                </ul>
+                <p class="main_tit_txt">마인드 피트니스</p>
+                <button type="button" class="btn btnSch"><span class="ico">검색</span></button>
+            </div>
+        </div>
+        <h3>메인헤더 1단형 독립형</h3>
+        <!-- header 1단형_독립형 ::  mainType 에 independence 추가-->
+        <div id="header" class="mainType independence">
+            <div class="header_inner line1">
+                <button type="button" class="btn allMenu"><span class="ico">전체메뉴</span><span class="ico new"></span></button>
+                <ul class="gnbMenu">
+                    <!-- selected class:on -->
+                    <li class="menu menu_home on">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">추천</a>
+                    </li>
+                    <li class="menu menu_exploration">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">탐색</a>
+                    </li>
+                    <li class="menu menu_subscription">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">구독</a>
+                    </li>
+                    <li class="menu menu_learn">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">학습</a>
+                    </li>     
+                </ul>
+                <p class="main_tit_txt">마인드 피트니스</p>
+                <button type="button" class="btn btnSch"><span class="ico">검색</span></button>
+            </div>
+        </div>
+
+        <!-- //메인헤더 1단형 -->
+        
+
+
+
+        <h3>메인헤더 2단형 아이콘</h3>
+        <!-- header 2단형 -->
+        <div id="header" class="mainType step2">
+            <div class="header_inner line1">
+                <button type="button" class="btn allMenu"><span class="ico">전체메뉴</span><span class="ico new"></span></button>  
+                <p class="main_tit_txt">SAMSUNG SDS</p>          
+                <button type="button" class="btn btnHome"><span class="ico">홈</span></button>
+            </div>            
+            <div class="header_inner line2">
+                <ul class="gnbMenu text">
+                    <!-- selected class:on -->
+                    <li class="menu menu_home on">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">추천</a>
+                    </li>
+                    <li class="menu menu_exploration">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">탐색</a>
+                    </li>
+                    <li class="menu menu_subscription">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">구독</a>
+                    </li>
+                    <li class="menu menu_learn">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">학습</a>
                     </li>
                 </ul>
             </div>
         </div>
         <!-- //header -->
+        <h3>메인헤더 2단형 텍스트</h3>
+        <!-- header 2단형 :: mainTpye2 에 txt 클래스 추가 -->
+        <div id="header" class="mainType step2 txt">
+            <div class="header_inner line1">
+                <button type="button" class="btn allMenu"><span class="ico">전체메뉴</span><span class="ico new"></span></button>  
+                <p class="main_tit_txt">SAMSUNG SDS</p>          
+                <button type="button" class="btn btnHome"><span class="ico">홈</span></button>
+            </div>            
+            <div class="header_inner line2">
+                <ul class="gnbMenu text">
+                    <!-- selected class:on -->
+                    <li class="menu menu_home on">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">추천</a>
+                    </li>
+                    <li class="menu menu_exploration">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">탐색</a>
+                    </li>
+                    <li class="menu menu_subscription">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">구독</a>
+                    </li>
+                    <li class="menu menu_learn">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">학습</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- //header -->
+
+
+
+
+
+
+
+
+
+        <h3>ATOM 헤더 2단형</h3>
+        <!-- header 2단형 -->
+        <div id="header" class="mainType2">
+            <div class="header_inner line1">
+                <button type="button" class="btn allMenu"><span class="ico">전체메뉴</span><span class="ico new"></span></button>  
+                <p class="main_tit_txt">ATORM</p>          
+                <button type="button" class="btn btnHome"><span class="ico">홈</span></button>
+            </div>            
+            <div class="header_inner line2">
+                <ul class="gnbMenu text">
+                    <!-- selected class:on -->
+                    <li class="menu menu_home on">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <span class="icon_desc">과정제작</span>
+                    </li>
+                    <li class="menu menu_exploration">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <span class="icon_desc">제작중</span>
+                    </li>
+                    <li class="menu menu_subscription">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <span class="icon_desc">과정운영관리</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
         
         <div id="content" class="etc">
           <div class="cell">
             <div class="inner">
-
-                <!-- list 임시 -->
-                <ul class="ulList type3">
-                    <li>
-                        <a href="#">삼성SDS<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성생명<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성증권<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성전자<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성SDS<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성생명<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성증권<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성전자<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성SDS<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성생명<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성증권<span class="ico gray_next"></span></a>
-                    </li>
-                    <li>
-                        <a href="#">삼성전자<span class="ico gray_next"></span></a>
-                    </li>
-                </ul>
-                <!-- //list -->
-                
             </div>
           </div>
         </div>
@@ -121,3 +247,8 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+    .sample_page .mainType{position:relative !important;margin-bottom:10px}
+    .sample_page .mainType2{position:relative !important;margin-bottom:10px}
+</style>
