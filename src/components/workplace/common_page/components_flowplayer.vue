13f832a0fbaf267334ba6747872f3be5b85fa25e
choks0225 <choks0225@gmail.com>
2019-01-28 14:23:39 +0900

    새 컴포넌트 업데이트

--- src/components/workplace/common_page/components_flowplayer.vue
+++ src/components/workplace/common_page/components_flowplayer.vue
@@ -268,29 +268,6 @@
             <div class="inner">
                 <p>내용1</p>
                 <p>내용2</p>
-                <slp-select placeholder="Select Charactor" width="auto">
-                    <optgroup label="Black Sails">
-                        <option value="flint">Flint</option>
-                        <option value="silver">Silver</option>
-                        <option value="vane">Vane</option>
-                        <option value="billy">Billy</option>
-                        <option value="jack">Jack</option>
-                    </optgroup>
-
-                    <optgroup label="Breaking Bad">
-                        <option value="heisenberg">Heisenberg</option>
-                        <option value="jesse">Jesse</option>
-                        <option value="saul">Saul</option>
-                        <option value="mike">Mike</option>
-                    </optgroup>
-
-                    <optgroup label="Game of Thrones">
-                        <option value="tyrion-lannister">Tyrion ㄴㅇㄻㄴㅇ ㄻㄴㅇㄹ ㅁㄴㅇ ㄻㄴㅇ asdf sad fasd fasad fsd fasd fasd fasd fㅁㄴㅇ ㄻㄴㅇㄹ Lannister</option>
-                        <option value="jamie-lannister">Jamie Lannister</option>
-                        <option value="daenerys-targaryen">Daenerys Targaryen</option>
-                        <option value="jon-snow">Jon Snow</option>
-                    </optgroup>
-                </slp-select>
                 <p>내용4</p>
                 <p>내용5</p>
                 <p>내용6</p>
