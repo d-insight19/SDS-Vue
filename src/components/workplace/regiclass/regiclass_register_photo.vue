6f5db92b7854dbe269f9fdb53cf47ab9b9009b6b
ssal07 <hm.kim@d-insight.co.kr>
2019-01-31 16:39:17 +0900

    Atorm_shortclip 추가

---
+++ src/components/workplace/regiclass/regiclass_register_photo.vue
@@ -0,0 +1,47 @@
+<template>
+ <!-- eslint-disable -->
+    <!-- wrap -->
+    <div id="wrap" class="colorCode2">        
+      <!-- container -->
+      <div id="container" class="mediaquery atorm atorm_confirmlist_detail">
+
+        <!-- header  -->
+        <div id="header" class="fixed">          
+          <div class="header_inner line1">
+            <button type="button" class="btn only close"><span class="ico">닫기</span></button>
+            <p class="header_text  font_18">사진 선택</p><button type="button" class="btn16 color_clr2">완료</button>
+          </div>
+        </div>
+        <!-- //header -->
+        <div id="content" class="etc">
+          <div class="cell">
+            <div class="inner">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- //container -->    
+
+    </div>
+    <!-- //wrap -->
+
+</template>
+
+<script>
+export default {
+  name: 'regiclass_register_photo',
+  /* vue lifecycle */
+  created () {
+  },
+  mounted () {
+  },
+  /* vue data */
+  data () {
+    return {
+    }
+  },
+  /* vue function */
+  methods: {
+  }
+}
+</script>
