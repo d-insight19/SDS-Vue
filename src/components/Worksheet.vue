c24a87251da59b1c1b9a0ce47cfbebf031436aca
symhw <hw.sim@d-insight.co.kr>
2019-02-08 10:28:47 +0900

    변경사항

--- src/components/Worksheet.vue
+++ src/components/Worksheet.vue
@@ -11604,7 +11604,7 @@
         <td class="textL"></td>
         <td>
           <router-link :to="{ name: 'components_snackbar' }">
-            components_snackbar
+            components_snackbar1
           </router-link>
         </td>
         <td></td>
