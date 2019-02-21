302818c7cff0148f1c8d3ea573451f661eac046c
1004wild <1004wild@naver.com>
2019-02-21 13:48:03 +0900

    bottomsheet 수정

--- src/components/workplace/study/Study_summary.vue
+++ src/components/workplace/study/Study_summary.vue
@@ -254,22 +254,22 @@
                         </div>
                         <span class="ico accordion_arrow"></span>
                     </li>
-                    <li class="open"  >
-                        <div class="download_detail border_clr1 clr1 wh">
+                    <li class="" :class="{ 'open' : cardList_show }" >
+                        <div class="download_detail border_clr1 ">
                             <div class="txt_wrap">
                                 <p class="icon_txt color_clr6">요약집</p>
                                 <p class="title">[원포인트 MBA]서울대 이경미 교수의 마케팅 실전전략</p>
                                 <ul class="info">
                                     <li>
-                                        <span class="mono_65 wh">핵심요약 3개</span>
+                                        <span class="mono_65">핵심요약 3개</span>
                                     </li>
                                     <li>
                                         <span class="ico clip"></span>
-                                        <span class="mono_65 wh">첨부파일</span>
+                                        <span class="mono_65">첨부파일</span>
                                     </li>
                                 </ul>
                             </div>
-                            <span class="ico accordion_arrow wh_up" :class="{ 'wh_down' : cardList_show }" @click="cardListToggle()"></span>
+                            <span class="ico accordion_arrow "  @click="cardListToggle()"></span>
                         </div>
 
                             <ul v-show="cardList_show">
