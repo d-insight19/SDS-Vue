dc21ffd8ed6822b076e0966161342abeba01f3ca
1004wild <1004wild@naver.com>
2019-02-22 09:26:02 +0900

    캘린더 수정

--- src/components/workplace/study/Study_home_set_calendar2.vue
+++ src/components/workplace/study/Study_home_set_calendar2.vue
@@ -10,6 +10,8 @@
                 <p class="header_text font_18">출석부</p>
             </div>
             <!-- //header -->      
+        
+        <!-- //header -->
 
             <div id="content" class="normal">
                 <div class="cell">
@@ -18,89 +20,37 @@
                     </div>
                 </div>
                 
-                <div class="cell">
-                    
-                </div>
             </div>
-            <!-- e: 동영상-->
 
-
-        </div>
-        <!-- //header -->
-
-        <div id="content" class="normal">
-
-          <div class="cell">
-            <div class="inner">
-                <p class="mb5">총 <span class="fwb ml2"><b>1,620</b></span></p>
-
-                <ul class="ulList type4">
-                    <li>
-                        <p class="title text-truncate">설문조사 타이틀은 한줄로 표기됩니다. 설문조사 타이틀은 한줄로 표기됩니다.설문조사 타이틀은 한줄로 표기됩니다.설문조사 타이틀은 한줄로 표기됩니다.설문조사 타이틀은 한줄로 표기됩니다.설문조사 타이틀은 한줄로 표기됩니다.</p>
-                        <div class="pipe-group">
-                            <p class="fwb mono_65"><span class="mr6">진행중</span><span class="date">2018.10.10</span></p>
-                            <p class="mt12"><span class="fwb color_clr3">참여완료</span></p>
-                        </div>
-                    </li>
-                    <li>
-                        <p class="title text-truncate">설문조사 타이틀은 한줄로 표기됩니다.</p>
-                        <div class="pipe-group">
-                            <p class="fwb mono_65"><span class="mr6">진행중</span><span class="date">2018.10.10</span></p>
-                            <p class="mt12"><span class="fwb mono_80">미참여</span></p>
-                        </div>
-                    </li>
-                    <li>
-                        <p class="title text-truncate">설문조사 타이틀은 한줄로 표기됩니다.</p>
-                        <div class="pipe-group">
-                            <p class="fwb mono_65"><span class="mr6">진행중</span><span class="date">2018.10.10</span></p>
-                            <p class="mt12"><span class="fwb color_clr3">참여완료</span></p>
-                        </div>
-                    </li>
-                    <li>
-                        <p class="title text-truncate">설문조사 타이틀은 한줄로 표기됩니다.</p>
-                        <div class="pipe-group">
-                            <p class="fwb mono_65"><span class="mr6">진행중</span><span class="date">2018.10.10</span></p>
-                            <p class="mt12"><span class="fwb color_clr3">참여완료</span></p>
-                        </div>
-                    </li>
-                    <li>
-                        <p class="title text-truncate">설문조사 타이틀은 한줄로 표기됩니다.</p>
-                        <div class="pipe-group">
-                            <p class="fwb mono_65"><span class="mr6">진행중</span><span class="date">2018.10.10</span></p>
-                            <p class="mt12"><span class="fwb color_clr3">참여완료</span></p>
-                        </div>
-                    </li>
-                </ul>
-
-                <div class="fixBtm" >
-                    <table class="">
-                        <tr>
-                            <td class="btm_tit ">
-                                2018년 9월 17일
-                            </td>                           
-                        </tr>
-                        <tr>
-                            <td>
-                                <span>입실</span>
-                                <span class="color_clr1 fwb600">10:30</span>
-                            </td>
-                            <td rowspan="2">
-                                <span class="color_clr1 fwb600">
-                                    2h 30m
-                                </span> /
-                                <span>
-                                    Total 3h 00m
-                                </span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td >
-                                <span>퇴실</span>
-                                <span class="color_clr1 fwb600">14:00</span>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
+            <div class="fixBtm" >
+                <table class="">
+                    <tr>
+                        <td class="btm_tit ">
+                            2018년 9월 17일
+                        </td>                           
+                    </tr>
+                    <tr>
+                        <td>
+                            <span>입실</span>
+                            <span class="color_clr1 fwb600">10:30</span>
+                        </td>
+                        <td rowspan="2">
+                            <span class="color_clr1 fwb600">
+                                2h 30m
+                            </span> /
+                            <span>
+                                Total 3h 00m
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td >
+                            <span>퇴실</span>
+                            <span class="color_clr1 fwb600">14:00</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
                 
         </div>
         <!-- //container --> 
