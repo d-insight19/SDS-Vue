bbda10177fa115e3a8135045ab8273e973ddc569
simhyewon <hw.sim@d-insight.co.kr>
2019-02-22 16:39:16 +0900

    test

---
+++ src/components/workplace/eduperson/Eduperson_loginOTP_2.vue
@@ -0,0 +1,88 @@
+<template>
+ <!-- eslint-disable -->
+    <!-- wrap -->
+    <div id="wrap" class="colorCode2">
+      <!-- container -->
+      <div id="container" class="mediaquery eduperson_login">
+        <!-- header -->
+        <div id="header" class="" >
+          <button type="button" class="btn only prev"><span class="ico">이전</span></button>
+        </div>
+        <!-- //header -->
+        <div id="content" class="normal">
+          <div class="cell">
+            <div class="inner">
+                <p class="edu_logintitle1">멀티캠퍼스</p>
+                <p class="edu_logintitle2">교육관리시스템</p>
+                <p class="label-tit-20 mb18">OTP 인증 선택</p>
+                <ul class="ul-block">
+                  <li>
+                      <label class="labelRdo">
+                          <input type="radio" class="rdo" name="test-rdo">
+                          <span class="ico">
+                              <span class="innerCircle"></span>
+                          </span>
+                          <span class="label">SMS</span>
+                      </label>
+                  </li>
+                  <li>
+                      <label class="labelRdo">
+                          <input type="radio" class="rdo" name="test-rdo" checked>
+                          <span class="ico">
+                              <span class="innerCircle"></span>
+                          </span>
+                          <span class="label">이메일</span>
+                      </label>
+                  </li>
+                </ul>
+
+                <div class="loginBtn">
+                    <button type="button" class="btn md clr1">OTP 생성</button>
+                </div>
+
+                <p class="label-tit-20" style="margin-top: 60px; margin-bottom:10px;">인증번호 입력</p>
+
+                <ul class="loginList">
+                    <li>
+                        <slp-text-field class="mb30" classObject="underline" style="position:relative"> <div class="time" style="position:absolute;top:0;right:0;color:red;">05:00</div></slp-text-field>
+                    </li>
+                </ul>
+
+                <div class="loginBtn">
+                    <button type="button" class="btn md clr1">OTP 인증</button>
+                </div>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+      <!-- //container -->
+    </div>
+    <!-- //wrap -->
+
+</template>
+
+<script>
+export default {
+  name: 'Eduperson_login',
+  /* vue lifecycle */
+  created () {
+  },
+  mounted () {
+  },
+  /* vue data */
+  data () {
+    return {
+      langFlag: false
+    }
+  },
+  /* vue function */
+  methods: {
+    changeLang () {
+      this.langFlag = !this.langFlag
+    }
+  }
+}
+</script>
---
+++ src/components/workplace/study/etc_keepstudy.vue
@@ -0,0 +1,224 @@
+<template>
+ <!-- eslint-disable -->
+    <!-- wrap -->
+    <div id="wrap" class="colorCode2">        
+      <!-- container -->
+      <div id="container" class="mediaquery MainType">
+
+        <!-- header 1단형 아이콘 -->
+        <div id="header" class="mainType">
+            <div class="header_inner line1">
+                <button type="button" class="btn allMenu"><span class="ico">전체메뉴</span><span class="ico new"></span></button>
+                <ul class="gnbMenu">
+                    <!-- selected class:on -->
+                    <li class="menu menu_star" :class="{'on': tabIdx == 1}" @click="onClick(1)">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">찜</a>
+                    </li>
+                    <li class="menu menu_exploration" :class="{'on': tabIdx == 2}" @click="onClick(2)">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">탐색</a>
+                    </li>
+                    <li class="menu menu_subscription" :class="{'on': tabIdx == 3}" @click="onClick(3)">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">구독</a>
+                    </li>
+                    <li class="menu menu_learn" :class="{'on': tabIdx == 4}" @click="onClick(4)" style="display:none;">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">학습</a>
+                    </li>                
+                </ul>
+                <p class="main_tit_txt">타이틀</p>
+                <button type="button" class="btn btnSch"><span class="ico">검색</span></button>
+            </div>
+            <div class="header_inner line2">
+                <ul class="gnbMenu">
+                    <!-- selected class:on -->
+                    <li class="menu menu_star on">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">찜</a>
+                    </li>
+                    <li class="menu menu_exploration">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">탐색</a>
+                    </li>
+                    <li class="menu menu_subscription">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">구독</a>
+                    </li>
+                    <li class="menu menu_learn">
+                        <a href="#" class="menu_icon"><span class="ico"></span></a>
+                        <a href="#" class="menu_txt">학습</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- //header -->
+
+        <div id="content" class="list">
+          <div class="cell">
+            
+            <!-- component -->
+            <div class="inner">
+                <h2 class="h-tit2 fwb600 mb10">학습 히스토리</h2>
+                <div class="scroll-x type2">
+                    <!-- list -->
+                    <ul class="thumList type2">
+                        <li>
+                            <div class="in">
+                            <a href="#" class="thum">
+                                <span class="label">정규</span>
+                                <span class="progWrap"><em class="bar clr2" style="width:50%;"></em></span>
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_m.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">만화로 배우는 알기 쉬운 경영 기초전략</a>
+                            <ul class="info">
+                                <li class="color_clr3">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                            </ul>
+                            <div class="tagWrap"><span>환급</span></div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="in">
+                            <a href="#" class="thum">
+                                <span class="label">정규</span>
+                                <span class="progWrap"><em class="bar clr2" style="width:50%;"></em></span>
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_m.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+
+                            <a href="#" class="tit">만화로 배우는 알기 쉬운 경영 기초전략</a>
+                            <ul class="info">
+                                <li class="color_clr3">이러닝</li>
+                            </ul>
+                            <div class="tagWrap"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="in">
+                            <a href="#" class="thum">
+                                <span class="label">정규</span>
+                                <span class="progWrap"><em class="bar clr2" style="width:50%;"></em></span>
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_m.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">만화로 배우는 알기 쉬운 경영 기초전략</a>
+                            <ul class="info">
+                                <li class="color_clr3">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                            </ul>
+                            <div class="tagWrap"><span>환급</span></div>
+                            </div>
+                        </li>
+                    </ul>
+                    <!-- //list -->
+                </div>
+            </div>
+
+                
+            <div class="inner">
+                <h2 class="h-tit2 fwb600 mb10">'마케팅직무'를 위한 추천과정</h2>
+                <!-- list -->
+                <ul class="thumList">
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <span class="label">정규</span>
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                            </ul>
+                            <div class="tagWrap"><span>환급</span><span>교재</span></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <span class="label">자율</span>
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                                <div class="tagWrap"></div>
+                            </a>
+                            <a href="#" class="tit">기초부터 예제까지 iOS 개발자를 위한 업무에 바로 쓰는 스플...</a>
+                            <div class="tagWrap"><span>이러닝</span></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <span class="label">정규</span>
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                            </ul>
+                            <div class="tagWrap"><span>환급</span></div>
+                        </div>
+                    </li>
+                </ul>
+                <!-- //list -->
+            </div>
+            <!-- //component -->
+
+          </div>
+        </div>
+
+        <!-- //quickWrite -->  
+        <div class="btn-quickWrite"></div>
+        <!-- //quickWrite -->
+        
+        <!-- toast -->  
+        <div class="toast">
+            <div class="in">
+                <div class="title_conents">
+                    <p class="toast_title">진행중인 학습</p>
+                    <p class="toast_contents">SPRINT - 구글 벤처스의 5일만에 기획부터 실행</p>
+                    <p class="toast_contents">4차 산업 혁명 시대의 경제심리, 행동 경제학에 주목 ...</p>
+                </div>  
+                <div class="toastBtnWrap">
+                    <a href="#">이어학습</a>
+                </div>
+            </div>
+        </div>
+        <!-- //toast -->
+
+      </div>
+      <!-- //container -->     
+    </div>
+    <!-- //wrap -->
+</template>
+
+<script>
+export default {
+  name: 'etc_keepstudy',
+  /* vue lifecycle */
+  created () {
+  },
+  mounted () {
+  },
+  /* vue data */
+  data () {
+    return {
+      tabIdx: 1
+    }
+  },
+  /* vue function */
+  methods: {
+  }
+}
+</script>
