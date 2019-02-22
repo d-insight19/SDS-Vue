bdebdd9356e25a92ae204d13740ca4c527e8008f
eh0130 <eh.jung@d-insight.co.kr>
2019-02-22 10:53:05 +0900

    0222 bpo 로그인관련 container class명 etc로 수정

--- src/components/workplace/bpo/BPO_Myinfo.vue
+++ src/components/workplace/bpo/BPO_Myinfo.vue
@@ -12,7 +12,7 @@
             </div>
             <!-- //header -->
 
-            <div id="content" class="normal">
+            <div id="content" class="etc">
                 <div class="cell">
                     <div class="inner">
                     <!-- myinfo_img -->
