f1c400e3f5eeb40d098d3dc09c3089cb0f16d662
ssal07 <hm.kim@d-insight.co.kr>
2019-02-22 11:06:46 +0900

    충돌수정

---
+++ src/components/workplace/eduperson/Eduperson_statement_searchword.vue
@@ -0,0 +1,294 @@
+<template>
+ <!-- eslint-disable -->
+    <!-- wrap -->
+    <div id="wrap" class="colorCode2">
+      <!-- container -->
+      <div id="container" class="mediaquery Eduperson_Operation">
+        <!-- header -->
+        <div id="header" class="fixed">
+          <button type="button" class="btn only prev"><span class="ico">이전</span></button>
+          <p class="header_text font_18">리더십 연관과정</p>
+        </div>
+        <!-- //header -->
+
+        <div id="content" class="normal">
+          <div class="cell">
+            <div class="inner">
+
+                <h2 class="h-tit2 fwb600" style="margin-bottom:14px">이러닝</h2>
+                <!-- list -->
+                <ul class="thumList">
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">만화로 배우는 알기 쉬운 경영 기초전략</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">업무에 바로 쓰는 경영경제 입문 과정</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="noline">
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">만화로 배우는 알기 쉬운 경영 기초전략</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+                <!-- //list -->
+
+                <p class="hr" style="margin:16px -24px 22px"></p>
+
+                <h2 class="h-tit2 fwb600" style="margin-bottom:14px">집합교육</h2>
+                <!-- list -->
+                <ul class="thumList">
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">만화로 배우는 알기 쉬운 경영 기초전략</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">업무에 바로 쓰는 경영경제 입문 과정</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="noline">
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">만화로 배우는 알기 쉬운 경영 기초전략</a>
+                            <a href="#" class="date">2018.10.01 ~2018.10.31</a>
+                            <ul class="info">
+                                <li class="color_clr3 fwb">이러닝</li>
+                                <li><span class="star ico"></span>4.5</li>
+                                <li>수료율 81.51%</li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+                <!-- //list -->
+
+                <p class="hr" style="margin:16px -24px 22px"></p>
+
+                 <h2 class="h-tit2 fwb600" style="margin-bottom:14px">자율</h2>
+                <!-- list -->
+                <ul class="thumList">
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">The Complete App Design Course - UX, UI an…</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">업무에 바로 쓰는 경영경제 입문 과정</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">초보자를 위한 업무에 바로 쓰는 자바 스크립트 입문 과정</a>
+                        </div>
+                    </li>
+                    <li class="noline">
+                        <div class="in">
+                            <a href="#" class="thum">
+                                <div class="thumbnail">
+                                    <img src="@/assets/img/thum_no_s.jpg" class="thumImg" alt="">
+                                </div>
+                            </a>
+                            <a href="#" class="tit">만화로 배우는 알기 쉬운 경영 기초전략</a>
+                        </div>
+                    </li>
+                </ul>
+                <!-- //list -->
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- //container -->
+
+
+    </div>
+    <!-- //wrap -->
+
+</template>
+
+<script>
+import EdupersonOperationSetGraph from './Eduperson_operation_set_graph'
+import EdupersonOperationSetTable from './Eduperson_operation_set_table'
+export default {
+  name: 'Eduperson_operation_set_control',
+  components: {'Eduperson_operation_set_graph': EdupersonOperationSetGraph, 'Eduperson_operation_set_table': EdupersonOperationSetTable},
+  /* vue lifecycle */
+  created () {
+  },
+  mounted () {
+  },
+  /* vue data */
+  data () {
+    return {
+      tabIdx: 1,
+      component: 'Eduperson_operation_set_graph'
+    }
+  },
+  /* vue function */
+  methods: {
+    onClick (idx) {
+      this.tabIdx = idx
+      if (idx === 1) {
+        this.component = 'Eduperson_operation_set_graph'
+      } else if (idx === 2) {
+        this.component = 'Eduperson_operation_set_table'
+      }
+    }
+  }
+}
+</script>
